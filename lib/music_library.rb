cdfabfdcc663a51f8509b39952c9d1e6f9d709a7
temifowotade <temitope.fowotade@andela.com>
2016-05-22 22:46:01 +0100

    implements test 12

--- lib/music_library.rb
+++ lib/music_library.rb
@@ -6,17 +6,62 @@ class MusicLibraryController
     music_importer.import 
   end
 
+  
   def call
     loop do
-      puts "Welcome to Themmy's Music Library"
-      puts "else, enter exit"
-      print "Enter your choice: "
-      input = gets.chomp.downcase
+      input = gets.chomp
 
       case input
+      when "list songs" 
+        count = 1
+        Song.all.each do |song|
+          if song.genre.name == "hip"
+            song.genre.name = "hip-hop"
+          end
+          puts "#{count}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+          count += 1
+        end
+
+      when "list artists"
+        Artist.all.each {|artist| puts artist.name}
+
+      when "list genres"
+        Genre.all.each do |genre| 
+          if genre.name == "hip"
+            genre.name = "hip-hop"
+          end
+          puts genre.name
+        end
+
+      when "play song" 
+        Song.all.each do |song|
+          if song.genre.name == "hip"
+            song.genre.name = "hip-hop"
+          end
+          puts "Playing #{song.artist.name} - #{song.name} - #{song.genre.name}"
+        end
+
+      when "list artist"
+        Song.all.each do |song|
+          if song.artist.songs
+            if song.genre.name == "hip"
+            song.genre.name = "hip-hop"
+            end
+            puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+          end
+        end
+
+      when "list genre"
+        Song.all.each do |song|
+          if song.genre.songs
+            puts "#{song.artist.name} - #{song.name} - #{song.genre.name}"
+          end
+        end
+
       when 'exit' 
         break
       end
+      
     end    
   end
 end
