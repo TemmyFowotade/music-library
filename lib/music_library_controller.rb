fae869522395897ca97350e3d9a038cd833a82bf
temifowotade <temitope.fowotade@andela.com>
2016-05-27 15:20:43 +0100

    Installed colorize

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,4 +1,5 @@
 require_relative './music_importer'
+require 'colorize'
 
 class MusicLibraryController
 
@@ -7,32 +8,44 @@ class MusicLibraryController
     music_importer.import
   end
 
+  def command_instructions
+    "Welcome to Themmy Music Library\n\n" +
+    "----------------------------------------------------------------------------\n\n" + 
+    "list songs: To list all songs in the music library\n" +
+    "play song: To play a song using it's serial number\n" +
+    "list artist: To list an artist's songs\n" +
+    "list genre: To list the songs under a specific genre\n" +
+    "list artists: To list all artists whose songs are saved in the music library\n" +
+    "list genres: To list all the genres that the songs belong to\n" +  "else, enter 'exit'."
+  end
+
   def call
     loop do
-      puts "Please enter command: "
+      puts command_instructions.colorize(:green)
+      print "Please enter command: ".colorize(:yellow)
       input = gets.chomp
       method_name = input.gsub(" ", "_")
       break if input == 'exit'
-      self.send(method_name)
+      send(method_name)
     end
   end
 
   def list_songs 
     Song.all.to_enum.with_index(1).each do |song, count|
-      puts "#{count}. #{song.to_s}"
+      puts "#{count}. #{song}"
     end
   end
 
-  def list_genre 
+  def list_genre
     print_model_songs(:genre)
   end 
 
-  def list_artist 
+  def list_artist
     print_model_songs(:artist)
   end 
 
   def play_song 
-    puts "Please enter song no: "
+    print "Please enter song no: ".colorize(:yellow)
     song_index = gets.chomp.to_i
     song_to_play = Song.all[song_index - 1]
     if song_to_play     
@@ -49,11 +62,12 @@ class MusicLibraryController
   end 
 
   def print_model_songs(model)
+    print "Please enter #{model}: ".colorize(:yellow)
+    user_input = gets.chomp
     Song.all.each do |song|
-      if song.send(model).songs
-        puts song.to_s
-      end
+      puts song.to_s if song.send(model).name == user_input 
     end
+    nil
   end
 
   def print_each_name(model)
@@ -66,7 +80,7 @@ class MusicLibraryController
     self.respond_to?(method_name, include_private) || super 
   end 
 
-  def method_missing(method_name, *args, &block)
+  def method_missing(_method_name, *_args)
     self.to_s 
   end 
   
